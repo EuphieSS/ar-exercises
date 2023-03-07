05623644e2db66727e6a19eec49a7fff7c8fbb02
EuphieSS <samma118@hotmail.com>
2023-03-07 05:15:57 +0000

    Create one to many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Juju", last_name: "Sur", hourly_rate: 100)
+@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store2.employees.create(first_name: "Troy", last_name: "Barnes", hourly_rate: 70)
+@store2.employees.create(first_name: "Abed", last_name: "Nadir", hourly_rate: 70)
+@store2.employees.create(first_name: "Jeff", last_name: "Winger", hourly_rate: 60)
